3f7cdb8095c356190f16feaddd1fe3315614474d
Gabriel <gabriel.felice@hotmail.com>
2025-06-15 21:28:28 -0300

    commit 2: memory_counter - refactoring and cleaning the code

---
+++ memory_counter/README.md.txt
@@ -0,0 +1,25 @@
+# Memory Module Calculator
+This is a simple project intended for learning.
+
+#Objective
+
+Study and Learning:
+
+- Input dada validation
+- Conditional logic and arithmetic
+- Functions creation and reuse
+- Good practices to keeping code clean
+- Code organization for posterior reading
+
+#How it works
+- Receive an input in GB 
+- Convert the value in GB to MB
+- Calcule how many modules of 4, 8, 16 and 32 GB are necessary.
+- Verify dual-channel possibilities
+- Verify motherboard possibilities
+
+Repository: thinking_python
+[gabriel-felice]https://github.com/gabriel-felice
+Archive memory_counter.py
+
+
--- memory_counter/memory_counter.py
+++ memory_counter/memory_counter.py
@@ -1,9 +1,10 @@
+#This app will calculate how much memory modules you'll need in your computer, preserving dual-channel or not.
 import time
-n = 1
-k = 0
+n = 1 #count variable
+k = 0 #count variable
 print("Let's calculate how much memory modules you'll need in your computer, preserving dual-channel.")
 time.sleep(1)
-while k > 64 or k == 0:
+while k > 64 or k == 0: #verify input
     while True:
         try:
             k = int(input("How much memory in GB you is gonna to take?(64gb max) "))
@@ -13,97 +14,53 @@ while k > 64 or k == 0:
     if k > 64 or k == 0:
         print('Must be less than 64 and different from 0.')
 time.sleep(1)
-kingb = 1024 * k
-if 8 <= k < 16:
+gb_to_MB = 1024 * k #convert GB to MB
+if 8 <= k < 16: #social interation
     print("That's a good amount.")
 elif  16<= k < 32:
     print("You're gonna to play cities skyline?")
 elif 32 <= k <= 64:
     print("WOWW! Let's go make a rocket!")
 time.sleep(1)
-while 2**n < kingb:
-    n += 1
-if 2**n >= kingb:
-    if 2**n <= 9999:
-        memrounded = 2**n//(int('1' + (int(len(str(2**n))) - 1) * '0'))
-    else:
-        memrounded = 2**n//(int('1' + (int(len(str(2**n))) - 2) * '0'))
-    print("You'll need", 2**n, "bits of memory.")
-    pent4 = memrounded // 4
-    pent8 = memrounded // 8
-    pent16 = memrounded // 16
-    pent32 = memrounded // 32
-
-    if pent4%2 == 0:
-        pent4 = memrounded // 4
-    else:
-        pent4 = str(memrounded // 4) + "(No Dual-Channel)"
-        paridade4 = False
-    if pent8%2 == 0:
-        pent8 = memrounded // 8
-    else:
-        pent8 = str(memrounded // 8) + "(No Dual-Channel)"
-        paridade8 = False
-    if pent16%2 == 0:
-        pent16 = memrounded // 16
-    else:
-        pent16 = str(memrounded // 16) + "(No Dual-Channel)"
-        paridade16 = False
-    if pent32%2 == 0:
-        pent32 = memrounded // 32
-    else:
-        pent32 = str(memrounded // 32) + "(No Dual-Channel)"
-        paridade32 = False
-    time.sleep(1)
-    try:
-        if pent4 + pent8 + pent16 + pent16 == 0:
-            print("It's better to buy nothing.")
+def how_much_modules(memory_in_MB): #return how much modules you'll need for 4, 16, 32 and 64.
+    p = [1024*4, 1024*8, 1024*16, 1024*32]
+    n = 0
+    modules_quantity = ['','','','']
+    for x in p:
+        modules_quantity[n] = memory_in_MB // p[n]
+        n += 1    
+    return modules_quantity
+def is_Dual_Channel_Capable(modules_quantity): #verify if the modules quantity is a pair number
+    dual_channel_capable = ['', '', '', '']
+    n = 0
+    for x in modules_quantity:
+        if modules_quantity[n] % 2 == 0:
+            dual_channel_capable[n] = True
         else:
-            print("You can buy:")
-    except:
-        print("You can buy:")
-
+            dual_channel_capable[n] = False
+        n += 1
+    return dual_channel_capable
+modules_quantity = how_much_modules(gb_to_MB) #modules quantity for each size in a lista 4 to 32
+is_dual_channel = is_Dual_Channel_Capable(modules_quantity) #if the modules quantity is pair, value in list is true, if not, is false.
+modules = [4, 8, 16, 32] #modules memory sizes
+if modules_quantity[0] >= 1: #if the 4gb modules number is less than 1 no make sense to buy anything.
+    print(f"You'll need {gb_to_MB} MegaBytes of memory.")
     time.sleep(1)
-    try:
-        if paridade4 == False:
-            print(f"{pent4} modules of 4gb.")
-            time.sleep(1)
-    except:
-        if pent4 > 4:
-            print(f"{pent4} modules of 4gb does'nt fit in any motherboard.")
-            time.sleep(1)
-        elif pent4 <=4 and pent4 != 0:
-            print(f"{pent4} modules of 4gb.")
-            time.sleep(1)
-    try:
-        if paridade8 == False:
-            print(f"{pent8} modules of 8gb.")
-            time.sleep(1)
-    except:
-        if pent8 > 4:
-            print(f"{pent8} modules of 8gb does'nt fit in any motherboard.")
-            time.sleep(1)
-        elif pent8 <= 4 and pent8 != 0:
-            print(f"{pent8} modules of 8gb.")
-            time.sleep(1)
-    try:
-        if paridade16 == False:
-            print(f"{pent16} modules of 16gb.")
-            time.sleep(1)
-    except:
-        if pent16 > 4:
-            print(f"{pent16} modules of 16gb does'nt fit in any motherboard.")
-            time.sleep(1)
-        elif pent16 <= 4 and pent16 != 0:
-            print(f"{pent16} modules of 16gb.")
-            time.sleep(1)
-    try:
-        if paridade32 == False:
-            print(f"{pent32} modules of 32gb.")
-            time.sleep(1)
-    except:
-        if pent32 >4:
-            print(f"{pent32} modules of 8gb does'nt fit in any motherboard.")
-            time.sleep(1)
-        elif pent32 <= 4 and pent32 != 0:
-            print(f"{pent32} modules of 32gb.")
+    print('You can buy: ')
+else:
+    print("It's better to buy nothing.")
+time.sleep(1)
+p = 0 #count variable. I don't know how to use enumerate yet, next att.
+for x in modules_quantity:
+    if modules_quantity[p] != 0:
+        if modules_quantity[p] <= 4:
+            if is_dual_channel[p] == True:
+                print(f'{modules_quantity[p]} modules of {modules[p]}gb, preserving Dual-Channel.')
+            else:
+                print(f'{modules_quantity[p]} modules of {modules[p]}gb, without Dual-Channel.')
+        else:
+            print(f'No support in any motherboard for {modules_quantity[p]} modules of {modules[p]}gb')
+    elif modules_quantity[p] > 4:
+        print('Is better to buy Nothing.')
+    p += 1
+    time.sleep(1)
